d5cfb9893dde086ac4994ec0b9fd429c5d89c424
Gaurav Khot <gauravkhot@yahoo.com>
2017-02-08 15:59:47 -0800

    Added check for email, password and confirm password to be present during create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable
+
+  validates :email, presence: true, on: :create
+  validates_uniqueness_of :email
+
+  validates :password, presence: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+
+  validates :password, confirmation: true, on: :create
+
 end
